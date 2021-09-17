00c808fa8cdbb9cfd31d520301e9e05bcdbec1b8
Ledenyinandi <ledenyinandi@gmail.com>
2021-09-17 16:50:56 +0200

    Create classes

---
+++ src/main/java/nandor/ledenyi/armyoop/MilitaryUnit.java
@@ -0,0 +1,10 @@
+package nandor.ledenyi.armyoop;
+
+public abstract class MilitaryUnit {
+
+    public abstract int getHealthPoints();
+
+    public abstract int doDamage();
+
+    public abstract void sufferDamage(int damage);
+}
